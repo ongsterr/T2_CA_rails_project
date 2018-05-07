08d6ccf7bfeb04dfb442a444b98345466d07165f
ongsterr <ong.chris11@gmail.com>
2018-05-07 15:32:49 +1000

    Initialize Devise and User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  
+  root 'pages#home'
+
+  # User controller using Devise
+  devise_for :users
+  devise_scope :user do
+      GET '/sign_up/user', to: 'devise/registrations#new'
+  end
+
   # Pages controller
   get 'pages/home'
   get 'pages/about'
