a855ab9e44cc776a0755517d4aabc7c0ac3e977a
ongsterr <ong.chris11@gmail.com>
2018-05-10 13:32:26 +1000

    Update omniauth initializer config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 
 # Complete this section with proper domain name once decided
-OmniAuth.config.full_host = Rails.env.production? ? 'https://domain.com' : 'http://localhost:3000'
+OmniAuth.config.full_host = Rails.env.production? ? 'https://desolate-plateau-51220.herokuapp.com' || 'http://desolate-plateau-51220.herokuapp.com' : 'http://localhost:3000'
 OmniAuth.config.logger = Rails.logger
