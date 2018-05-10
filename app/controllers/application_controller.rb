2c406c704474ce1326c76594024c7c5a1b4b5455
ongsterr <ong.chris11@gmail.com>
2018-05-11 07:59:40 +1000

    Update user controller to include role in strong parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  end
         
   private
       def user_not_authorized
