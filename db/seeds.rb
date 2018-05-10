0952abf6a03251778325930db710ec8785db892e
ongsterr <ong.chris11@gmail.com>
2018-05-11 09:23:37 +1000

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,78 +7,126 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Below seeded data for testing
-admin_user = {
-    email: ENV.fetch('ADMIN_EMAIL'),
-    password: ENV.fetch('ADMIN_PASSWORD')
-}
+# admin_user = {
+#     email: ENV.fetch('ADMIN_EMAIL'),
+#     password: ENV.fetch('ADMIN_PASSWORD')
+# }
 
-uploader = ImageUploader.new(:store)
-file_1 = File.new(Rails.root.join('app/assets/images/profile_image.jpg'))
-uploaded_file_1 = uploader.upload(file_1)
+# test_users = [
+#     {
+#     email: "test1@user.com",
+#     password: "foobar123",
+#     role: "Transporter"
+#     },
+#     {
+#     email: "test2@user.com",
+#     password: "foobar123",
+#     role: "Transporter"
+#     },
+#     {
+#     email: "test3@user.com",
+#     password: "foobar123",
+#     role: "Transporter"
+#     },
+#     {
+#     email: "test4@user.com",
+#     password: "foobar123",
+#     role: "Transporter"
+#     },
+#     {
+#     email: "test5@user.com",
+#     password: "foobar123",
+#     role: "Transporter"
+#     },
+#     {
+#     email: "test6@user.com",
+#     password: "foobar123",
+#     role: "Transporter"
+#     },
+#     {
+#     email: "test7@user.com",
+#     password: "foobar123",
+#     role: "Transporter"
+#     },
+#     {
+#     email: "test8@user.com",
+#     password: "foobar123",
+#     role: "Transporter"
+#     }
+# ]
 
-file_2 = File.new(Rails.root.join('app/assets/images/pitch.png'))
-uploaded_file_2 = uploader.upload(file_2)
 
-file_3 = File.new(Rails.root.join('app/assets/images/T2_logo.png'))
-uploaded_file_3 = uploader.upload(file_3)
+# uploader = ImageUploader.new(:store)
+# file_1 = File.new(Rails.root.join('app/assets/images/profile_image.jpg'))
+# uploaded_file_1 = uploader.upload(file_1)
 
-file_4 = File.new(Rails.root.join('app/assets/images/jazz.jpg'))
-uploaded_file_4 = uploader.upload(file_4)
+# file_2 = File.new(Rails.root.join('app/assets/images/pitch.png'))
+# uploaded_file_2 = uploader.upload(file_2)
 
-admin_profile = {
-    user_id: "1",
-    first_name: "Christopher",
-    last_name: "Ong",
-    date_of_birth: "10/01/1990",
-    mobile_phone: "0422678500",
-    about_me: "I love travelling!",
-    language_1: "English",
-    language_2: "Chinese",
-    language_3: "Malay",
-    profile_image_data: uploaded_file_1.to_json,
-    addr_unit_number: "1",
-    addr_street_number: "888",
-    addr_street_name: "Collins Street",
-    addr_city: "Docklands",
-    addr_state: "Victoria",
-    addr_postcode: "3008",
-    addr_country_code: "AU",
-    passport_number: "A123456",
-    passport_expiry_date: "12/12/2022",
-    passport_country_code: "AU",
-    passport_image_data: uploaded_file_2.to_json,
-    drivers_license_number: "A123456",
-    drivers_license_expiry_date: "12/12/2022",
-    drivers_license_country_code: "AU",
-    drivers_license_image_data: uploaded_file_3.to_json,
-    verified?: 0
-}
+# file_3 = File.new(Rails.root.join('app/assets/images/T2_logo.png'))
+# uploaded_file_3 = uploader.upload(file_3)
 
-test_product = {
-    transporter_id: "1",
-    price_per_day: 20000,
-    vehicle_model: "Honda Jazz",
-    vehicle_capacity: 5,
-    vehicle_description: "Super spacious and comfortable!",
-    vehicle_image_data: uploaded_file_4.to_json,
-    cancellation_policy: "Full refund only if cancellation is 45 days before trip.",
-    things_to_note: "Just be ready to have fun!",
-    location_id: "1"
-}
+# file_4 = File.new(Rails.root.join('app/assets/images/jazz.jpg'))
+# uploaded_file_4 = uploader.upload(file_4)
 
-test_booking = {
-    product_id: "1",
-    transporter_id: "1",
-    traveler_id: "1",
-    booking_ref_number: "ABC123",
-    booking_date_from: "15/06/2018",
-    booking_date_to: "18/06/2018",
-    number_of_traveler: 4,
-    transporter_cost: 20000,
-    platform_cost: 4000,
-    total_cost: 24000,
-    booking_confirmed?: true
-}
+# 7.times do |n|
+#     test_profile = {
+#         user_id: "#{n+1}",
+#         first_name: "User",
+#         last_name: "#{n+1}",
+#         date_of_birth: "10/01/1990",
+#         mobile_phone: "0412345678",
+#         about_me: "I love travelling!",
+#         language_1: "English",
+#         language_2: "Chinese",
+#         language_3: "Malay",
+#         profile_image_data: uploaded_file_1.to_json,
+#         addr_unit_number: "1",
+#         addr_street_number: "888",
+#         addr_street_name: "Collins Street",
+#         addr_city: "Docklands",
+#         addr_state: "Victoria",
+#         addr_postcode: "3008",
+#         addr_country_code: "AU",
+#         passport_number: "A123456",
+#         passport_expiry_date: "12/12/2022",
+#         passport_country_code: "AU",
+#         passport_image_data: uploaded_file_2.to_json,
+#         drivers_license_number: "A123456",
+#         drivers_license_expiry_date: "12/12/2022",
+#         drivers_license_country_code: "AU",
+#         drivers_license_image_data: uploaded_file_3.to_json,
+#         verified?: 0
+#         }
+#     Profile.create!(admin_profile) {p "Admin profile created"}
+# end
+
+
+# test_product = {
+#     transporter_id: "1",
+#     price_per_day: 20000,
+#     vehicle_model: "Honda Jazz",
+#     vehicle_capacity: 5,
+#     vehicle_description: "Super spacious and comfortable!",
+#     vehicle_image_data: uploaded_file_4.to_json,
+#     cancellation_policy: "Full refund only if cancellation is 45 days before trip.",
+#     things_to_note: "Just be ready to have fun!",
+#     location_id: "1"
+# }
+
+# test_booking = {
+#     product_id: "1",
+#     transporter_id: "1",
+#     traveler_id: "1",
+#     booking_ref_number: "ABC123",
+#     booking_date_from: "15/06/2018",
+#     booking_date_to: "18/06/2018",
+#     number_of_traveler: 4,
+#     transporter_cost: 20000,
+#     platform_cost: 4000,
+#     total_cost: 24000,
+#     booking_confirmed?: true
+# }
 
 location = [
     {
@@ -118,17 +166,17 @@ location = [
     }
 ]
 
-test_review = {
-    booking_id: "1",
-    reviewer_id: "1",
-    reviewed_id: "2",
-    review: "Chris has been super good!",
-    rating: 4
-}
+# test_review = {
+#     booking_id: "1",
+#     reviewer_id: "1",
+#     reviewed_id: "2",
+#     review: "Chris has been super good!",
+#     rating: 4
+# }
 
-User.create!(admin_user) {p "Admin user created"}
-Profile.create!(admin_profile) {p "Admin profile created"}
+# User.create!(admin_user) {p "Admin user created"}
+# Profile.create!(admin_profile) {p "Admin profile created"}
 Location.create!(location) {p "Product location seeded"}
-Product.create!(test_product) {p "Product created"}
-Booking.create!(test_booking) {p "Booking completed"}
-Review.create!(test_review) {p "Test review created"}
+# Product.create!(test_product) {p "Product created"}
+# Booking.create!(test_booking) {p "Booking completed"}
+# Review.create!(test_review) {p "Test review created"}
